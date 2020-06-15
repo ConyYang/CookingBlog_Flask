b3467f3165bf51cf982076115522016a187069c4
Yang Yubei <yangyubei0218@gmail.com>
2020-06-15 16:25:32 +0800

    forge

--- cooking/__init__.py
+++ cooking/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
 from cooking.settings import configuration
 import os
 from cooking.blueprints.admin import admin_bp
 from cooking.blueprints.cook import cook_bp
 from cooking.blueprints.auth import auth_bp
 from cooking.extensions import *
+import click
 
 
 def make_app(config_name=None):
@@ -14,15 +16,73 @@ def make_app(config_name=None):
     app = Flask('Cooklog')
     app.config.from_object(configuration[config_name])
 
+    register_blue_prints(app)  # blueprints
+    register_commands(app)  # customer shell commands
+    register_errors(app)  # error handler function
+    register_extensions(app)  # extension init
+    register_logging(app)  # log
+    register_shell_context(app)  # shell context
+    register_template_context(app)  # template context
+    return app
+
+
+def register_blue_prints(app):
     app.register_blueprint(cook_bp)  # front end
     app.register_blueprint(auth_bp, )  # authenticate account
     app.register_blueprint(admin_bp, url_prefix='/admin')  # back end
 
+
+def register_extensions(app):
     bootstrap.init_app(app)
     db.init_app(app)
     moment.init_app(app)
     ckeditor.init_app(app)
     mail.init_app(app)
 
-    return app
 
+def register_logging(app):
+    pass
+
+
+def register_shell_context(app):
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db)
+
+
+def register_template_context(app):
+    pass
+
+
+def register_errors(app):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('errors/40-0.html'), 400
+
+
+def register_commands(app):
+    @app.cli.command()
+    @click.option('--category', default=7, help='No. of Cooking Categories, default is 7.')
+    @click.option('--recipe', default=10, help='No. of recipes, default is 10.')
+    @click.option('--comment', default=80, help='No of comments, defualt is 80')
+    def forge(category, recipe, comment):
+        """
+        Generate half fake database
+        """
+        from cooking.fakes import fake_admin, fake_comments, fake_recipes, add_categories
+        db.drop_all()
+        db.create_all()
+
+        click.echo('Generating the administrator...')
+        fake_admin()
+
+        click.echo('Generating %d categories...' % category)
+        add_categories(category)
+
+        click.echo('Generating %d recipes...' % recipe)
+        fake_recipes(recipe)
+
+        click.echo('Generating %d comments...' % comment)
+        fake_comments(comment)
+
+        click.echo('Done')
--- cooking/fakes.py
+++ cooking/fakes.py
@@ -1,1 +1,129 @@
 # create fake stats
+from faker import Faker
+
+from cooking.models import *
+from cooking.extensions import db
+from django.db import IntegrityError
+
+import random
+
+faker = Faker()
+
+
+def fake_admin():
+    admin_cony = Admin(
+        username='Cony Admin',
+        blog_title='Cooking Blog',
+        blog_sub_title='This is my first Cooking Blog !',
+        name='Yu Cony',
+        about='A COOKING programmer'
+    )
+    db.session.add(admin_cony)
+    db.session.commit()
+
+
+category_list = ['Si Chuan', 'Cantonese', 'Western',
+                 'Thailand', 'Japanese', 'French',
+                 'Dessert']
+
+
+def add_categories(count=7):
+    category = Category(name='default')
+    db.session.add(category)
+
+    for i in range(count):
+        category = Category(category_list[i])
+        db.session.add(category)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+
+titles_list = ['Twice Cooked Spicy Pork Slices',
+               'ClayPot Rice',
+               'Tomato Pasta',
+               'Curried crab',
+               'Beef Ramen',
+               'Baked Snail',
+               'Tiramisu',
+               'Hot Garlic Source Eggplant',
+               'Char siew',
+               'Onion Steak']
+
+
+def fake_recipes(count=10):
+    for i in range(count):
+        post = Post(
+            title=titles_list[i],
+            content=faker.text(),
+            categories=category_list[i % 7],
+            timestamp=faker.date_time_this_year()
+        )
+        db.session.add(post)
+    db.session.commit()
+
+
+def fake_comments(count=80):
+    # Reviewed comments
+    for i in range(count):
+        comment = Comment(
+            author=faker.name(),
+            email=faker.email(),
+            site=faker.url(),
+            content=faker.sentence(),
+            timestamp=faker.date_time_this_year(),
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+    # Not reviewed Comments
+    notReviewed_comments = int(count*0.1)
+    for i in range(notReviewed_comments):
+        comment = Comment(
+            author=faker.name(),
+            email=faker.email(),
+            site=faker.url(),
+            content=faker.sentence(),
+            timestamp=faker.date_time_this_year(),
+            reviewed=False,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+    # Admin Comments
+    admin_comments = int(count*0.05)
+    for i in range(admin_comments):
+        comment = Comment(
+            author='Cony Yang',
+            email='ConyYang@cooking.com',
+            site='cooking.com',
+            content=faker.sentence(),
+            timestamp=faker.date_time_this_year(),
+            from_admin=True,
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+    db.session.commit()
+
+    # Reply
+    for i in range(count*0.15):
+        comment = Comment(
+            author=faker.name(),
+            email=faker.email(),
+            site=faker.url(),
+            content=faker.sentence(),
+            timestamp=faker.date_time_this_year(),
+            reviewed=True,
+            replied=Comment.query.get(random.randint(1, Comment.query.count())),
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
+
+
+
